b760b7f74095898e511c97e37b5b212b426b3076
Jarek <101186879+jarek167714@users.noreply.github.com>
2022-05-17 11:17:12 +0000

    Commit 3_1

--- Main.java
+++ Main.java
@@ -1,18 +1,41 @@
 import java.io.IOException;
+import java.util.Scanner;
 
 class Main {
-  public static void main(String[] args) {
-    try {
-      Service1 s = new Service1();
-      s.addStudent(new Student("Krzysztof", 20));
-      s.addStudent(new Student("Janusz", 40));
-
-      var students = s.getStudents();
-      for(Student current : students) {
-        System.out.println(current.ToString());
+  public static void main(String[] args) throws IOException {
+    Scanner scanner = new Scanner(System.in);
+    boolean shouldContinue = true;
+    while(shouldContinue){
+      System.out.println("Aby dodać studenta wybierz 1");
+      System.out.println("Aby wyjść z programu wyberz 0");
+      int userChoice = scanner.nextInt();
+      switch(userChoice){
+        case 1:
+          case1();
+          System.out.println("Dopisano studenta, powrót do menu\n");
+          break;
+        case 0:
+          System.out.println("Do zobaczenia");
+          shouldContinue = false;
+          break;
       }
-    } catch (IOException e) {
-
     }
+    }
+    
+    private static void case1() throws IOException 
+    {
+      
+    
+      Scanner scanner = new Scanner(System.in);
+      System.out.println("Podaj imię: ");
+
+      String imie = scanner.nextLine();
+
+      System.out.println("Podaj wiek: ");
+
+      int wiek = scanner.nextInt();
+      Service1 s = new Service1();
+
+      s.addStudent(new Student(imie, wiek));
   }
 }
