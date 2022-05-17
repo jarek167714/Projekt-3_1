c04f6db39047b2c7a4062d48638e60d4b032ad89
Jarek <101186879+jarek167714@users.noreply.github.com>
2022-05-17 11:44:16 +0000

    3_1b

--- Main.java
+++ Main.java
@@ -8,6 +8,7 @@ class Main {
     while(shouldContinue){
       System.out.println("Aby dodać studenta wybierz 1");
       System.out.println("Aby wyjść z programu wyberz 0");
+      System.out.println("Aby wypisać wszystkich studentów wciśnij 2")
       int userChoice = scanner.nextInt();
       switch(userChoice){
         case 1:
@@ -18,24 +19,38 @@ class Main {
           System.out.println("Do zobaczenia");
           shouldContinue = false;
           break;
+           case 2:
+          {
+          try
+          {
+          Service1 = new Service1();
+          var students = s.getStudents();
+          for(Student current : students)
+          {
+            System.out.println(current.ToString());
+          }
+         break;
+        }  
+         }
       }
     }
     }
     
     private static void case1() throws IOException 
     {
-      
-    
       Scanner scanner = new Scanner(System.in);
       System.out.println("Podaj imię: ");
-
       String imie = scanner.nextLine();
-
       System.out.println("Podaj wiek: ");
-
       int wiek = scanner.nextInt();
+      System.out.println("Podaj nazwisko: ");
+      String nazwisko = scanner.nextLine();
+      
       Service1 s = new Service1();
-
-      s.addStudent(new Student(imie, wiek));
+      s.addStudent(new Student(imie, wiek, nazwisko));
+      
   }
+ 
 }
+}
+
